b87cb71ffddf94b075268dcb4f8111228569f45a
Dru Macasieb <79002260+neodru@users.noreply.github.com>
2021-10-18 15:16:12 -0700

    Ch3Ex4.cpp

---
+++ week 1/Ch3Ex4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+// reference page 168
+{
+    double cost;
+    double area;
+    double bagSize;
+
+    cout << fixed << showpoint << setprecision(2);
+    
+    cout << "Enter the amount of fertilizer, in pounds, "
+        << "in one bag: "; 
+    cin >> bagSize;
+    cout << endl;
+    
+    cout << "Enter the cost of the " << bagSize
+        << " pound fertilizer bag: ";
+    cin >> cost;
+    cout << endl;
+        
+    cout << "Enter the area, in square feet, that can be "
+         << "fertilized by one bag: ";
+    cin >> area;
+    cout << endl;
+        
+    cout << "The cost of the fertilizer per pound is: $"
+        << bagSize / cost << endl;
+        
+    cout << "The cost of fertilizing per square foot is: $"
+            << cost / area << endl; // area / cost is incorrect
+    
+    return 0; 
+
+}
+        
+       
+  
